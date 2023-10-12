43eb4771e033fb03688500675ee14d17fa1a9dd3
jhonthe77 <jhonthe7@gmail.com>
2023-10-12 14:28:09 -0500

    hole0.7

--- visualizacion_analisis.py
+++ visualizacion_analisis.py
@@ -48,7 +48,7 @@ def visualizacion_analisis():
 
     with col2:
         df_agente=df_.groupby('Agente')['Calificación de satisfacción'].mean().reset_index()
-        fig=px.pie(df_agente,names='Agente',values='Calificación de satisfacción',color='Agente',hole=0.5,
+        fig=px.pie(df_agente,names='Agente',values='Calificación de satisfacción',color='Agente',hole=0.7,
                 title='Promedio de la Calificacion de cada Agente')
         st.plotly_chart(fig,use_container_width=True,theme=theme)
 
