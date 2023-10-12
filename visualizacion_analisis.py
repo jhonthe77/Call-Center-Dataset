b911268bf45b450871f9f2bc4c516f2bb309e62f
jhonthe77 <jhonthe7@gmail.com>
2023-10-12 11:22:24 -0500

    nuevo grafico

--- visualizacion_analisis.py
+++ visualizacion_analisis.py
@@ -5,9 +5,18 @@ import streamlit as st
 
 
 def visualizacion_analisis():
+    col1, col2=st.columns(2)
     theme=None
-    df_=pd.read_csv('Callcenter.csv')
-    df_agente=df_.groupby('Agente')['Calificación de satisfacción'].mean().reset_index()
-    fig=px.bar(df_agente,x='Agente',y='Calificación de satisfacción',color='Agente',
-               title='Promedio de la Calificacion de cada Agente')
-    st.plotly_chart(fig,use_container_width=True,theme=theme)
+    with col1:
+        df_=pd.read_csv('Callcenter.csv')
+        df_agente=df_.groupby('Agente')['Calificación de satisfacción'].mean().reset_index()
+        fig=px.bar(df_agente,x='Agente',y='Calificación de satisfacción',color='Agente',
+                title='Promedio de la Calificacion de cada Agente')
+        st.plotly_chart(fig,use_container_width=True,theme=theme)
+
+    with col2:
+        df_=pd.read_csv('Callcenter.csv')
+        df_agente=df_.groupby('Agente')['Calificación de satisfacción'].mean().reset_index()
+        fig=px.pie(df_agente,names='Agente',values='Calificación de satisfacción',color='Agente',
+                title='Promedio de la Calificacion de cada Agente')
+        st.plotly_chart(fig,use_container_width=True,theme=theme)
